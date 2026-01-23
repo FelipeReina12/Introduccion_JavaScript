b34bc56ac4bc7c36fdd1fc31d7fb4ac741b22a2f
FelipeReina12 <jfreinap@academia.usbbog.edu.co>
2026-01-23 09:05:58 -0500

    Repasando temas

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -103,3 +103,174 @@ const persona = {
     }
 }
 persona.saludar("Hola, buenos dias")
+
+
+// Arrays
+// Crear un array con los videojugos favoritos 
+const videojuegos = [
+    {nombre: "The Legend of Zelda: Breath of the wild", plataforma: "Nintendo Switch", año: 2017},
+    {nombre: "Super Mario Odyssey", plataforma: "Nintendo Switch", año: 2017},
+    {nombre: "Grand Theft Auto V", plataforma: "Multiplataforma", año: 2013},
+    {nombre: "The Witcher 3: Wild Hunt", plataforma: "Multiplataforma", año: 2015},
+    {nombre: "Red Dead Redemption 2", plataforma: "Multiplataforma", año: 2018},
+    {nombre: "Minecraft", plataforma: "Xbox, Pc", año: 2011},
+]
+// console.log(videojuegos);
+
+// Verificar si un videojuego existe en el array
+videojuegos.forEach (juego => {
+    if(juego.nombre == "Minecraft"){
+        console.log("Minecraft si está descargado");
+    }
+});
+
+// Filtrar videojuegos que sean de Nintendo Switch
+const nintendo = videojuegos.filter(juego => juego.plataforma === "Nintendo Switch");
+console.log(nintendo);
+
+// Estructura de control
+const dinero = 910;
+
+if(dinero >= 1000){
+    console.log("Tienes suficientes fondos para realizar la compra");
+}else{
+    console.log("Saldo insuficiente para realizar la compra");
+}
+
+const universidad = "Estudiante";
+let pago = 5000;
+
+if(universidad === "Director"){
+    console.log(`El pago para el ${universidad} es de ${pago}`);
+} else if(universidad === "Decano"){
+    console.log(`El pago para el ${universidad} es de ${pago + 1000}`);
+} else if(universidad === "Estudiante"){
+    let matricula = 900;
+    console.log(`El ${universidad} no recibe pago, al contrario debe pagar ${matricula} de matricula`)
+} else {
+    console.log("No pertenece a la universidad");
+}
+
+// comparación con switch
+const musico = "saxofonista";
+switch(musico) {
+    case "guitarrista":
+        console.log("Tienes ensayo el dia lunes");
+        break;
+    case "bajista":
+        console.log("Tienes ensayo el dia martes");
+        break;
+    case "saxofonista":
+        console.log("Tienes ensayo el dia miércoles");
+        break;
+    default:
+        console.log("No tienes ensayos esta semana");
+}
+
+// for Loop
+// Identificar números pares del 0 al 100
+for(let num = 0; num <= 100; num++){
+    if(num % 2 === 0){
+        console.log(`El número ${num} es par`)
+    } else {
+        console.log(`El número ${num} es impar`);
+    }
+}
+
+//Recorrer el array de videojuegos e imprimir sus nombres
+for(let j = 0; j < videojuegos.length; j++){
+    console.log(videojuegos[j].nombre);
+}
+
+//While Loop
+// Tabla de multiplicar del 5
+let contador = 1;
+
+while(contador <= 10){
+    console.log(`5 x ${contador} = ${5 * contador}`);
+    contador++;
+}
+
+let potencia = 2;
+
+while(potencia <=20){
+    console.log(`La potencia de ${potencia} es ${potencia * potencia}`);
+    potencia++;
+}
+
+// Uso de This en objetos
+const empresa = {
+    nombre: "Tech Solutions",
+    empleados: 150,
+    capital: 500000,
+    ubicacion: "Bogotá",
+    info: function(){
+        console.log(`La empresa ${this.nombre} tiene ${this.empleados} empleados, un capital de ${this.capital} y está ubicada en ${this.ubicacion}`);
+    }
+}
+empresa.info();
+
+// Programación Orientada a Objetos (POO)
+// Object constructor 
+function Clase(titulo, duracion, precio){
+    this.titulo = titulo;
+    this.duracion = duracion;
+    this.precio = precio;
+}
+// Agregamos una propiedad al objeto
+const clase1 = new Clase("JavaScript desde cero", "8 horas", 12);
+const clase2 = new Clase("HTML y CSS", "6 horas", 10);
+
+// Con prototype agregamos un método al objeto
+Clase.prototype.detallesClase = function() {
+    return `La clase ${this.titulo} tiene una duración de ${this.duracion} y cuesta $${this.precio}`;
+}
+console.log(clase1.detallesClase());
+console.log(clase2.detallesClase());
+
+// Clases en JavaScript
+class Apple {
+    constructor(modelo, almacenamiento, precio){
+        this.modelo = modelo;
+        this.almacenamiento = almacenamiento;
+        this.precio = precio;
+    }
+    formatearApple(){
+        return `El modelo ${this.modelo} tiene un almacenamiento de ${this.almacenamiento} y cuesta $${this.precio}`;
+    }
+}
+const iphone = new Apple("Iphone 13 Pro", "256GB", 999);
+const ipad = new Apple("Ipad Air", "128GB", 599);
+const macbook = new Apple("Macbook Pro", "1TB", 1250);
+
+console.log(iphone.formatearApple());
+console.log(ipad.formatearApple());
+console.log(macbook.formatearApple());
+
+// Herencia
+//Heredamos el constructor y métodos de otra clase 
+class MercadoLibre extends Apple {
+    constructor(modelo, almacenamiento, precio, tienda){
+        super(modelo, almacenamiento, precio);
+        this.tienda = tienda;
+    }
+    formatearApple(){
+        return `${super.formatearApple()} y está disponible en la tienda ${this.tienda}`;
+    }
+}
+const productoMl = new MercadoLibre("Iphone 17 Pro Max", "1TB", 1599, "WillTech");
+console.log(productoMl.formatearApple());
+
+
+// Promise
+const registroUsuario = new Promise( (resolve, reject) => {
+    const registrado = false;
+    if(registrado){
+        resolve("Usuario registrado exitosamente");
+    } else {
+        reject("Error al registrar el usuario");
+    }
+});
+registroUsuario
+    .then(mensaje => console.log(mensaje))
+    .catch(error => console.log(error))
